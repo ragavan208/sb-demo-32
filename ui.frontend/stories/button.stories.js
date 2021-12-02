d8b305fd11f3fb03f9fd9c545e8582de6f5383a3
Seenivasa Ragavan <mailtoragavan.be@gmail.com>
2021-12-02 20:41:04 +1100

    initial

---
+++ ui.frontend/stories/button.stories.js
@@ -0,0 +1,52 @@
+/**
+ * Storybook stories for the carousel component
+ */
+
+import { fetchFromAEM } from 'storybook-aem-wrappers';
+import { aemMetadata } from '@storybook/aem';
+import { StyleSystem } from 'storybook-aem-style-system';
+import { Grid } from 'storybook-aem-grid';
+
+export default {
+    title: 'Components/button',
+    decorators: [
+        aemMetadata({
+            decorationTag: {
+                cssClasses: ['button', 'component', StyleSystem, Grid],
+                tagName: 'div'
+            }
+        })
+    ],
+};
+
+
+const emptyContentPath = "/content/sb-demo-32-design-system/button/jcr:content/root/container/container/empty";
+export const empty = () => ({
+    template: async () => fetchFromAEM(emptyContentPath)
+});
+empty.story = {
+    name: 'Empty Story',
+    parameters: {}
+};
+
+
+
+const shortTextContentPath = "/content/sb-demo-32-design-system/button/jcr:content/root/container/container/shorttext";
+export const shortText = () => ({
+    template: async () => fetchFromAEM(shortTextContentPath)
+});
+shortText.story = {
+    name: 'Short Text Story',
+    parameters: {}
+};
+
+
+
+const longTextContentPath = "/content/sb-demo-32-design-system/button/jcr:content/root/container/container/longtext";
+export const longText = () => ({
+    template: async () => fetchFromAEM(longTextContentPath)
+});
+longText.story = {
+    name: 'Long Text Story',
+    parameters: {}
+};
